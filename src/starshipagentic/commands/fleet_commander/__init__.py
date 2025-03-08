926c93627194f6406730d4cfe7800d6cd3f4bc2d
tsomerville2 <48032495+tsomerville2@users.noreply.github.com>
2025-03-07 21:51:28 -0800

    refactor: Simplify run_group() by removing complex argument handling logic

--- src/starshipagentic/commands/fleet_commander/__init__.py
+++ src/starshipagentic/commands/fleet_commander/__init__.py
@@ -19,19 +19,5 @@ def run_group():
     """Entry point for running the fleet_commander command group directly."""
     import sys
     from starshipagentic.cli import main as cli_main
-    original_args = sys.argv.copy()
-    
-    # When run as a standalone command (e.g., 'fleet_commander visualize-ship')
-    # we need to transform it to 'starshipagentic fleet_commander visualize-ship'
-    if len(original_args) > 1:
-        # The command name is in original_args[0], subcommands start at index 1
-        command_name = original_args[0]
-        subcommands = original_args[1:]
-        
-        # If the first argument is a subcommand of this group, preserve it
-        sys.argv = ['starshipagentic', 'fleet_commander'] + subcommands
-    else:
-        # No subcommands, just show the group help
-        sys.argv = ['starshipagentic', 'fleet_commander']
-    
+    sys.argv = ['starshipagentic', 'fleet_commander']
     cli_main()
